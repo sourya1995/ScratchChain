9ee5300dfff4655bbf7f9b90b964dc4e9748ff08
Sourya Bhattacharya <souryabhattacharya21@gmail.com>
2024-07-07 13:06:06 +0530

    Initialize Blockchain

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,3 +1,15 @@
+package blockchain
+
 type Blockchain struct {
 	Blocks []*Block
+}
+
+func InitBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{Genesis()}}
+}
+
+func (chain *Blockchain) AddBlock(data string) {
+	prevBlock := chain.Blocks[len(chain.Blocks)-1]
+	newBlock := CreateBlock(data, prevBlock.Hash)
+	chain.Blocks = append(chain.Blocks, newBlock)
 }
